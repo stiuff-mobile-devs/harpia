939fef285bd3722fb770221eecfe6e27e098d2ac
João Vítor <jvlgoncalves@id.uff.br>
2026-08-07 12:05:35 -0300

    Fix: corrigido erro que acontecia quando o usuário dava zoom out rápido demais.

--- lib/app/modules/monitora_uff/controller/tracking_controller.dart
+++ lib/app/modules/monitora_uff/controller/tracking_controller.dart
@@ -266,6 +266,10 @@ class TrackingController extends GetxController with WidgetsBindingObserver {
 
   Future<void> centerMapOnCurrentLocation() async {
     try {
+      if (!position.latitude.isFinite || !position.longitude.isFinite) {
+        return;
+      }
+
       mapController.move(LatLng(position.latitude, position.longitude), 15.0);
     } catch (e) {
       if (kDebugMode) print('Error moving map: $e');
